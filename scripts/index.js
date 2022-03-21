027a695ae6fe8bf6f59cc11e45317d770aa6df9c
ProudNeron <neronnp4@gmail.com>
2022-03-21 19:59:57 +0700

    Corrections after second review

--- scripts/index.js
+++ scripts/index.js
@@ -5,8 +5,8 @@ const editBtn = document.querySelector('.profile__edit-button');
 
 let userName = document.querySelector('.profile__user-name');
 let aboutUser = document.querySelector('.profile__about-user');
-let inputName = popup.querySelector('.text-input_user-name');
-let inputAboutUser = popup.querySelector('.text-input_user-about');
+let inputName = popup.querySelector('.text-input_user_name');
+let inputAboutUser = popup.querySelector('.text-input_user_about');
 
 function openPopup() {
   inputName.value = userName.textContent;
@@ -15,19 +15,19 @@ function openPopup() {
 }
 
 function closePopup(evt) {
-  evt.preventDefault();
   if (evt.target === evt.currentTarget || evt.target === closedBtn)
   {
     popup.classList.remove('popup_opened');
   }
-  if (evt.target === popup.querySelector('.popup__save-btn'))
-  {
-    userName.textContent = inputName.value;
-    aboutUser.textContent = inputAboutUser.value;
-    popup.classList.remove('popup_opened');
-  }
+}
+
+function sendForm(evt) {
+  evt.preventDefault();
+  userName.textContent = inputName.value;
+  aboutUser.textContent = inputAboutUser.value;
+  popup.classList.remove('popup_opened');
 }
 
 editBtn.addEventListener('click', openPopup);
 popup.addEventListener('click', closePopup);
-form.addEventListener('submit', closePopup);
+form.addEventListener('submit', sendForm);
