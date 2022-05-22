7462d9f48e98a31999a7851105ca711c327834ba
ProudNeron <neronnp4@gmail.com>
2022-05-23 01:53:50 +0700

    fix building for cross OS

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
@@ -12,6 +13,7 @@ module.exports = {
     new HtmlWebpackPlugin({
     template: path.resolve(__dirname, 'src', 'index.html')
   }),
+    new CleanWebpackPlugin(),
     new MiniCssExtractPlugin(),
   ],
   mode: 'development',
